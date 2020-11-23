5c133a9766bf8d048a96fcfadc3106cfc4c7f36b
Nathan Pham <nathan-pham.me@gmail.com>
2020-11-23 12:43:55 -0800

    rollup

---
+++ rollup.config.js
@@ -0,0 +1,57 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import { terser } from "rollup-plugin-terser";
+
+import * as path from "path";
+
+const dist = path.resolve("./dist");
+
+let plugins = [];
+
+plugins.push(
+  ...[
+    json(),
+    resolve({
+      preferBuiltins: true,
+    }),
+    commonjs(),
+  ]
+);
+
+if (!("dev" == (process.env.mode && process.env.mode.toLowerCase()))) {
+  plugins.push(
+    ...[
+      terser({
+        compress: {
+          arrows: false,
+          keep_classnames: true,
+          keep_fnames: true,
+          keep_infinity: true,
+          typeofs: false,
+        },
+        sourcemap: true,
+        keep_classnames: true,
+        keep_fnames: true,
+      }),
+    ]
+  );
+}
+
+export default {
+  input: path.join("index.js"),
+  output: [
+    {
+      file: path.join(dist, "cjs", "bundle.js"),
+      format: "cjs",
+      sourcemap: true,
+    },
+    {
+      file: path.join(dist, "esm", "bundle.js"),
+      format: "es",
+      sourcemap: true,
+    },
+  ],
+  plugins,
+  external: ["http", "https"],
+};
