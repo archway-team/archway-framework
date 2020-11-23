1db383d59dfb86608f03cebcc44d1cea4ed8edf9
Nathan Pham <nathan-pham.me@gmail.com>
2020-11-23 15:16:38 -0800

    build settings

--- rollup.config.js
+++ rollup.config.js
@@ -5,9 +5,11 @@ import { terser } from "rollup-plugin-terser";
 
 import * as path from "path";
 
-const dist = path.resolve("./dist");
+const distname = "./dist";
+const pkgname = "./pkg";
 
 let plugins = [];
+let builds = [];
 
 plugins.push(
   ...[
@@ -37,20 +39,46 @@ if (!("dev" == (process.env.mode && process.env.mode.toLowerCase()))) {
   );
 }
 
-export default {
-  input: path.join("index.js"),
+/* archway-framework */
+builds.push({
+  input: "index.js",
   output: [
     {
-      file: path.join(dist, "cjs", "bundle.js"),
+      file: path.join(distname, "core/cjs", "index.js"),
       format: "cjs",
       sourcemap: true,
     },
     {
-      file: path.join(dist, "esm", "bundle.js"),
+      file: path.join(distname, "core/esm", "index.mjs"),
       format: "es",
       sourcemap: true,
     },
   ],
   plugins,
   external: ["http", "https"],
-};
+});
+
+/* archway/pkgs */
+const pkgs = ["html", "navigation"];
+
+for (const pkg of pkgs) {
+  builds.push({
+    input: path.join(pkgname, pkg, "index.js"),
+    output: [
+      {
+        file: path.join(distname, pkg, "cjs", "index.js"),
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: path.join(distname, pkg, "esm", "index.mjs"),
+        format: "es",
+        sourcemap: true,
+      },
+    ],
+    plugins,
+    external: ["http", "https"],
+  });
+}
+
+export default builds;
